8dedcbc23fb79a3ef2d7da43e5657d3bce4d5e9b
agungh21 <agungmuhammad70@gmail.com>
2021-11-21 13:44:29 +0700

    + tambah user modal

--- index.php
+++ index.php
@@ -20,6 +20,15 @@
                 <div class="row">
                     <div class="col-md-2"></div>
                     <div class="col-md-8">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahUserModal">
+                            + User
+                        </button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
                         <table id="datatable" class="table">
                             <thead>
                                 <th>No</th>
@@ -73,6 +82,53 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
+    <!-- Modal Tambah User -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="tambahUserModal" tabindex="-1" aria-labelledby="tambahUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="tambahUserModalLabel">Tambah User</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="javascript:void();" method="post" id="formTambahUser">
+                    <div class="modal-body">
+                        <div class="mb-3 row">
+                            <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="username" class="form-control" id="inputUsername" value="">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="email" class="form-control" id="inputEmail" value="">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputMobile" class="col-sm-2 col-form-label">mobile</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="mobile" class="form-control" id="inputMobile" value="">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputCity" class="col-sm-2 col-form-label">City</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="city" class="form-control" id="inputCity" value="">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Modal Tambah User -->
 </body>
 
 </html>
